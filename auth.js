75f432e5ac1806c3d80f67115a4e7bc5ed562b4d
Lanaa12321 <lana.aldaeni@ucdenver.edu>
2026-03-01 22:32:42 -0700

    test

--- auth.js
+++ auth.js
@@ -1,19 +1,10 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 
-// loads .env (safe even if server.js also loads it)
-require('dotenv').config();
-
 passport.use(new BasicStrategy(
   function(username, password, done) {
-
     const expectedUser = process.env.BASIC_USER;
     const expectedPass = process.env.BASIC_PASS;
-
-    // optional: if env vars missing, fail clearly
-    if (!expectedUser || !expectedPass) {
-      return done(null, false);
-    }
 
     if (username === expectedUser && password === expectedPass) {
       return done(null, { name: username });
